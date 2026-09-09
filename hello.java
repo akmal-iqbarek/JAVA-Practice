2a5ec3c19f70e1afb38db5fe3e48ec01330ec5a8
akmal-iqbarek <akmaliqbal2004@gmail.com>
2026-09-09 19:59:16 +0530

    Day1: Variables, Date Types

---
+++ hello.java
@@ -0,0 +1,16 @@
+class hello{
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        int container=78;
+        String AkmalIqbarek = "AK";
+        String container2=AkmalIqbarek;
+        System.out.println(container);
+        System.out.print(container2);
+
+        int a = 10;
+        int b= 34;
+        int c = 45;
+        int total= a+b+c;
+        System.out.println(total);
+    } 
+}
