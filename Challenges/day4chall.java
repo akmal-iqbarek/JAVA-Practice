f7d29f59573da807ad9e36c5cb9533d5c41f991d
akmal-iqbarek <akmaliqbal2004@gmail.com>
2026-09-20 12:28:32 +0530

    Day4 completed IF ELSE

---
+++ Challenges/day4chall.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class day4chall{
+    public static void main(String args[]){
+        /*
+        int num1 = 50;
+        int num2 = 50;
+        
+        if(num1==num2){
+            System.out.println(num1+" is equals to "+num2);
+        }
+        else{
+            System.out.println(num1+" is not equals to "+num2);
+        }
+        
+
+        Scanner myscan = new Scanner(System.in);
+        System.out.println("Enter Num1:");
+        int num1 = myscan.nextInt();
+        System.out.println("Enter Num2:");
+        int num2 = myscan.nextInt();
+
+        if(num1==num2){
+            System.out.println(num1+" is equals to "+num2);
+        }
+        else{
+            System.out.println(num1+" is not equals to "+num2);
+        }
+        
+
+
+       Scanner myscan = new Scanner(System.in);
+       String RCB = myscan.nextLine();
+
+       String Value1 = "win";
+       String Value2 = "loose";
+
+       if(RCB.equals(Value1)){
+        System.out.println("ee saala cup namdhe");
+       }
+       else if(RCB.equals(Value2)){
+        System.out.println("cup illa");
+       }
+       else{
+        System.out.println("wrong input");
+       }
+       */
+      Scanner myscan = new Scanner(System.in);
+      String meghana = myscan.nextLine();
+
+      String Value = "dead";
+      if(meghana.equals(Value)){
+            System.out.println("Meghana is dead");}
+
+      else{
+            System.out.println("surya weds meghana");
+        }
+      }
+
+    }
+
